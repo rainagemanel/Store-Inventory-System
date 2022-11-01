27a4f6bf411d1a4970d7d80400f4a4eccb34e5ab
raina-gemanel-lspu <raina.gemanel@lspu.edu.ph>
2022-11-02 05:52:02 +0800

    123

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,session,g,url_for
 from models import db,UniformModel
- 
-app = Flask(__name__)
- 
+
+
+import os
+
+
+app = Flask(__name__, template_folder='./templates',static_url_path='', static_folder='./static')
+
+app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -10,11 +15,38 @@ db.init_app(app)
 @app.before_first_request
 def create_table():
     db.create_all()
- 
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user', None)
+
+        if request.form['password'] == 'password':
+            session['user'] = request.form['username']
+            return redirect(url_for('protected'))
+
+    return render_template(login.html)
+
+
+@app.route("/index")
+def index():
+    if g.user:
+        return render_template('index.html',user=session['user'])
+    return redirect(url_for('login'))
+
+@app.route("/about")
+def about():
+    if g.user:
+        return render_template('about.html',user=session['user'])
+    return redirect(url_for('login'))
+
 @app.route('/create' , methods = ['GET','POST'])
 def create():
+    if g.user:
+        return render_template('create.html',user=session['user'])
+    
     if request.method == 'GET':
-        return render_template('createpage.html')
+        return render_template('create.html')
  
     if request.method == 'POST':
         dep_name = request.form['dep_name']
@@ -32,15 +64,17 @@ def create():
         )
         db.session.add(uniforms)
         db.session.commit()
-        return redirect('/')
+        return redirect('/index')
+    return redirect(url_for('login'))
  
- 
-@app.route('/')
+@app.route('/datalist')
 def RetrieveList():
     uniforms = UniformModel.query.all()
-    return render_template('datalist.html',uniforms = uniforms)
- 
-@app.route('/<int:id>/edit',methods = ['GET','POST'])
+    if g.user:
+        return render_template('datalist.html',user=session['user'],uniforms = uniforms)
+    return redirect(url_for('login'))
+    
+@app.route('/<int:id>/edit', methods = ['GET','POST'])
 def update(id):
     uniform = UniformModel.query.filter_by(id=id).first()
 
@@ -64,23 +98,23 @@ def update(id):
         )
         db.session.add(uniform)
         db.session.commit()
-        return redirect('/')
-        return f"Uniform with id = {id} Does nit exist"
+        return redirect('/index')
  
     return render_template('update.html', uniform = uniform)
  
  
 @app.route('/<int:id>/delete', methods=['GET','POST'])
 def delete(id):
+
     uniforms = UniformModel.query.filter_by(id=id).first()
     if request.method == 'POST':
         if uniforms:
             db.session.delete(uniforms)
             db.session.commit()
-            return redirect('/')
+            return redirect('/index')
             
-     
     return render_template('delete.html')
  
-app.run(host='localhost', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
 
